d74659ba308fa905255e2f1e16601021e36bf89b
Adam Steinberg <126136311+javascriptforlife66@users.noreply.github.com>
2023-09-30 12:31:59 -0500

    hello

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("pop-out-nav").style.top = "0";
-    } else {
-        document.getElementById("pop-out-nav").style.top = "-50px";
-    }
-}
